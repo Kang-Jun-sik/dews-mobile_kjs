88cc4b448c9211aadc4186f060f648d8601b82df
노휘겸 <whistle@douzone.com>
2020-12-02 20:42:22 +0900

    날짜 형식에 대한 환경 설정 형식 및 기본 값 생성

--- src/app/env/GlobalEnvironmentVariables.ts
+++ src/app/env/GlobalEnvironmentVariables.ts
@@ -1,5 +1,45 @@
+type ValueAndDisplayFormat = {
+  value: string;
+  display: string;
+};
+
 export interface GlobalEnvironmentVariables {
   format: {
+    date?: {
+      date?: ValueAndDisplayFormat;
+      dateTime?: ValueAndDisplayFormat;
+      dateMonth?: ValueAndDisplayFormat;
+      dateWeek?: ValueAndDisplayFormat;
+      time?: ValueAndDisplayFormat;
+    };
     number?: string;
   };
 }
+
+export const DefaultGlobalEnvironmentVariables: GlobalEnvironmentVariables = {
+  format: {
+    date: {
+      date: {
+        value: 'yyyyMMdd',
+        display: 'yyyy-MM-dd'
+      },
+      dateTime: {
+        value: 'yyyyMMddHHmmss',
+        display: 'yyyy-MM-dd HH:mm:ss'
+      },
+      dateMonth: {
+        value: 'yyyyMM',
+        display: 'yyyy-MM'
+      },
+      dateWeek: {
+        value: 'yyyyWW',
+        display: 'yyyy-WW'
+      },
+      time: {
+        value: 'HHmmss',
+        display: 'HH:mm:ss'
+      }
+    },
+    number: '#,###.##'
+  }
+};
