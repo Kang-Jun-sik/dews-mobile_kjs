f13085ce59207cd3dd229d04616f41e87cc74d08
dpthf403 <dpthf403@douzone.com>
2020-12-10 10:10:48 +0900

    PD1-1795-Snackbar 컴포넌트 개발

--- stories/dews-snackbar.stories.ts
+++ stories/dews-snackbar.stories.ts
@@ -1,10 +1,119 @@
 import { html } from 'lit-html';
 import '../src/dews-mobile.js';
+// eslint-disable-next-line import/extensions
+import { Snackbar, SNACKBAR_OPTIONS } from '../src/components/snackbar/snackbar';
 
 export default {
   title: 'Snackbar'
 };
 
-export const Snackbar = () => html`<div style="width: 360px">
-  <dews-snackbar></dews-snackbar>
+export const Snackbar1 = () => html`<div style="width: 360px">
+  <p style="height: 200px;">
+    <button
+      @click="${() => {
+        showSnackbar('승인신청이 접수되었습니다.').done(() => {
+          console.log('승인 done');
+        });
+      }}"
+    >
+      승인
+    </button>
+  </p>
+  <p style="height: 200px;">
+    <button
+      @click="${() => {
+        showSnackbar('저장되었습니다.', false).done(() => {
+          console.log('아이콘 미출력 done');
+        });
+      }}"
+    >
+      아이콘 미출력
+    </button>
+  </p>
+  <p style="height: 200px;">
+    <button
+      @click="${() => {
+        showSnackbar('대기중인 업데이트 정보가 있습니다.', 'info').done(() => {
+          console.log('정보1 done');
+        });
+      }}"
+    >
+      정보1
+    </button>
+  </p>
+  <p style="height: 200px;">
+    <button
+      @click="${() => {
+        showSnackbar('준비중입니다.', { icon: 'info' }).done(() => {
+          console.log('정보2 done');
+        });
+      }}"
+    >
+      정보2
+    </button>
+  </p>
+  <p style="height: 200px;">
+    <button
+      @click="${() => {
+        showSnackbar('잘못 입력하셨습니다.', 'warning').done(() => {
+          console.log('경고 done');
+        });
+      }}"
+    >
+      경고
+    </button>
+  </p>
+  <p style="height: 200px;">
+    <button
+      @click="${() => {
+        showSnackbar('조건에 해당하는 데이터가 없습니다.', { icon: 'warning', exposureTime: 5 }).done(() => {
+          console.log('경고 5초 done');
+        });
+      }}"
+    >
+      경고 5초
+    </button>
+  </p>
+  <p style="height: 200px;">
+    <button
+      @click="${() => {
+        showSnackbar('치명적인 버그가 발생했습니다.', 'error').done(() => {
+          console.log('에러 done');
+        });
+      }}"
+    >
+      에러
+    </button>
+  </p>
+  <p style="height: 200px;">
+    <button
+      @click="${() => {
+        showSnackbar('작업이 처리되지 않았습니다.', { icon: 'error', exposureTime: 7 }).done(() => {
+          console.log('에러 7초 done');
+        });
+      }}"
+    >
+      에러 7초
+    </button>
+  </p>
+  <p style="height: 200px;">
+    <button
+      @click="${() => {
+        showSnackbar('다운로드를 준비하고 있습니다.', 'loading').done(() => {
+          console.log('로딩 done');
+        });
+      }}"
+    >
+      로딩
+    </button>
+  </p>
+  <!--  <dews-snackbar></dews-snackbar>-->
 </div>`;
+
+function showSnackbar(message: string, options?: SNACKBAR_OPTIONS) {
+  const sb = new Snackbar();
+  sb.message = message;
+  sb.options = options;
+  sb.showSnackBar();
+  return sb;
+}
