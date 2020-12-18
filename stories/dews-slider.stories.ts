0755de86804e071bb2f5269176bc654573a4ce7b
dpthf403 <dpthf403@douzone.com>
2020-12-18 09:24:26 +0900

    PD1-1824-Slider 개발 완료

---
+++ stories/dews-slider.stories.ts
@@ -0,0 +1,17 @@
+import { html } from 'lit-html';
+import '../src/dews-mobile.js';
+
+export default {
+  title: 'Slider'
+};
+
+export const Slider = () => html`<div>
+  <p style="padding: 5px; background-color: bisque">범위 슬라이더 예시 (기본)</p>
+  <dews-slider></dews-slider>
+  <p style="padding: 5px; background-color: bisque">범위 슬라이더 예시</p>
+  <dews-slider slider-type="range" max="20" min="0" step="4"></dews-slider>
+  <p style="padding: 5px; background-color: bisque">스텝퍼 슬라이더 예시</p>
+  <dews-slider slider-type="stepper" max="20" min="0" step="4" value="4"></dews-slider>
+  <p style="padding: 5px; background-color: bisque">스텝퍼 슬라이더 예시 (hideText)</p>
+  <dews-slider slider-type="stepper" max="20" min="0" step="4" hideText></dews-slider>
+</div>`;
