04af544f90405ac8b6664766211981532340f2d0
구영준 <kuyoungjun@douzone.com>
2021-01-07 15:34:48 +0900

    [PD1-1888]-텍스트박스: 패스워드 타입 추가

--- stories/dews-textbox.stories.ts
+++ stories/dews-textbox.stories.ts
@@ -15,6 +15,7 @@ export const TextBox = () => html`<div style="width: 360px">
         <dews-textbox title="readonly" readonly></dews-textbox>
         <dews-textbox title="disabled readonly" disabled readonly></dews-textbox>
         <dews-textbox title="placeholder" placeholder="placeholder"></dews-textbox>
+        <dews-textbox type="password" title="password"></dews-textbox>
       </container-content>
     </dews-search-container>
   </dews-box>
