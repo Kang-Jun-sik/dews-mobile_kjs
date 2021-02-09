85ce8bf3b3cb81648d90c75336df3a9785511be0
구영준 <kuyoungjun@douzone.com>
2021-02-09 16:57:35 +0900

    폼컨테이너 data-set 기능 추가

--- src/components/containerbutton/containerbutton.ts
+++ src/components/containerbutton/containerbutton.ts
@@ -1,11 +1,18 @@
-import { DewsLayoutComponent } from '../base/DewsLayoutComponent.js';
 import { html, internalProperty, property, TemplateResult } from 'lit-element';
-
 import template from './containerbutton.html';
 import scss from './containerbutton.scss';
-import { SearchContainer } from '../searchcontainer/searchcontainer.js';
+import { DataSet, SearchContainer, SearchData } from '../searchcontainer/searchcontainer.js';
+import { FormContainer } from '../formcontainer/formcontainer.js';
+import { Tooltip } from '../tooltip/tooltip.js';
+import { Messagebox } from '../messagebox/dews-messagebox.js';
+import { date } from '@dews/dews-mobile-core';
+import DrawerRightBase from '../base/DrawerRightBase.js';
+import { query } from 'lit-element/lib/decorators';
 
-export class Containerbutton extends DewsLayoutComponent {
+const userId = 'kuyoungjun';
+const menuId = 'MA0001';
+
+export class Containerbutton extends DrawerRightBase {
   static styles = scss;
 
   @property({ type: Boolean, attribute: 'data-set' })
@@ -23,23 +30,54 @@ export class Containerbutton extends DewsLayoutComponent {
   @internalProperty()
   _buttonList: Array<TemplateResult> = [];
 
-  private $parent: SearchContainer | undefined;
+  @internalProperty()
+  private _dataSetHeader: Array<TemplateResult> = [];
+
+  @internalProperty()
+  private _dataSetList: Array<TemplateResult> = [];
+
+  private $parent: SearchContainer | FormContainer | undefined;
+
+  private contentList: Array<TemplateResult> = [];
+
+  @query('.dews-snackbar.dataset')
+  snackbar: HTMLElement | undefined;
+
+  @query('.dataset-nodata')
+  datasetNodata: HTMLElement | undefined;
 
   connectedCallback() {
     super.connectedCallback();
-
-    if (this.parentElement?.tagName === 'DEWS-SEARCH-CONTAINER') {
-      this.$parent = this.parentElement as SearchContainer;
+    this._iconList = [];
+    let list: any;
+    switch (this.parentElement?.tagName) {
+      case 'DEWS-SEARCH-CONTAINER':
+        this.$parent = this.parentElement as SearchContainer;
+        list = this.$parent!.querySelector('container-content')?.children;
+        for (let i = 0; i < list!.length; i++) {
+          this.contentList.push(list[i]);
+        }
+        break;
+      case 'DEWS-FORM-CONTAINER':
+        this.$parent = this.parentElement as FormContainer;
+        list = this.$parent.querySelector('container-content')?.querySelectorAll('form-section');
+        for (let i = 0; i < list.length; i++) {
+          for (let j = 0; j < list[i]!.children.length; j++) {
+            this.contentList.push(list[i].children[j]);
+          }
+        }
+        break;
     }
+
     if (this.setButton) {
       this._iconList.push(html` <li class="data-set">
-        <button class="set" @click="${this.$parent!._setClick.bind(this.$parent)}"><span>Data Set</span></button>
+        <button class="set" @click="${this._setClick}"><span>Data Set</span></button>
       </li>`);
     }
     if (this.captureButton) {
       this._iconList.push(
         html` <li class="data-capture">
-          <button class="capture" @click="${this.$parent!._captureClick.bind(this.$parent)}">
+          <button class="capture" @click="${this._captureClick}">
             <span>Data Capture</span>
           </button>
         </li>`
@@ -48,11 +86,264 @@ export class Containerbutton extends DewsLayoutComponent {
     if (this.resetButton) {
       this._iconList.push(
         html` <li class="data-reset">
-          <button class="reset" @click="${this.$parent!._resetClick.bind(this.$parent)}">
+          <button class="reset" @click="${this._resetClick}">
             <span>Data Reset</span>
           </button>
         </li>`
       );
+    }
+  }
+
+  /**
+   * set 버튼 클릭시 처리
+   */
+  public _setClick() {
+    this._renderDataList();
+    this._open();
+  }
+
+  private _renderDataList() {
+    let dataSetList;
+    let isDataSet = false;
+
+    if (localStorage.dataSet) {
+      dataSetList = JSON.parse(localStorage.dataSet);
+    }
+
+    this._dataSetList = [];
+    this._dataSetHeader = [];
+
+    if (dataSetList) {
+      for (let i = 0; i < dataSetList.length; i++) {
+        const dataSet = dataSetList[i];
+        if (dataSet.containerId === this.$parent?.id) {
+          isDataSet = true;
+          dataSet.data.sort((a: SearchData[], b: SearchData[]) => {
+            return new Date(b[0].dateTime).getTime() - new Date(a[0].dateTime).getTime();
+          });
+
+          this._dataSetHeader.push(html`
+            <div class="dataset-total">
+              <span>총 <strong>${dataSet.data.length}</strong>건</span>
+            </div>
+
+            <div class="dataset-total-delete">
+              <dews-button
+                type="text"
+                text="전체삭제"
+                size="small"
+                @click="${this._removeAllData.bind(this)}"
+              ></dews-button>
+            </div>
+          `);
+
+          this.datasetNodata!.style.display = 'none';
+
+          for (let i = 0; i < dataSet.data.length; i++) {
+            const data = dataSet.data[i];
+            let value = false;
+            for (let j = 0; j < data.length; j++) {
+              const item: SearchData = data[j];
+              if (item.value) {
+                value = true;
+                break;
+              }
+            }
+            this._dataSetList.push(
+              html`
+                <div class="dataset">
+                  <div class="header">
+                    <span class="date">${data[0].dateTime}</span>
+                    <button class="apply-button" @click="${this._setData.bind(this, data)}">적용</button>
+                    <button class="delete-button" @click="${this._reomveData.bind(this, dataSet.data, i)}">
+                      <span>삭제</span>
+                    </button>
+                  </div>
+                  <div class="field">
+                    ${value
+                      ? data.map((item: SearchData) =>
+                          item.value
+                            ? html`<button @click="${(e: any) => this._showToolTip(e.target, item.title)}">
+                                ${item.value}
+                              </button>`
+                            : html``
+                        )
+                      : html`<span class="nodata">데이터가 없습니다.</span>`}
+                  </div>
+                </div>
+              `
+            );
+          }
+        }
+      }
+      // <span class="nodata">데이터가 없습니다.</span>
+      // </div>
+      if (!isDataSet) {
+        this.datasetNodata!.style.display = 'block';
+      }
+    } else {
+      this.datasetNodata!.style.display = 'block';
+    }
+  }
+
+  private _showToolTip(target: HTMLElement, title: string) {
+    if (title) {
+      const tooltip = new Tooltip();
+      tooltip.options = {
+        type: 'normal',
+        text: title,
+        position: 'bottom',
+        fadeOutTime: 0
+      };
+      tooltip._target = target;
+      tooltip.show();
+    }
+  }
+
+  /**
+   * data를 search container에 적용합니다.
+   * @param data
+   * @private
+   */
+  private _setData(data: SearchData[]) {
+    for (let i = 0; i < this.contentList.length; i++) {
+      const control: any = this.contentList[i];
+
+      for (let j = 0; j < data.length; j++) {
+        if (control.id && control.id === data[j].id) {
+          control.value = data[j].value;
+        }
+      }
+    }
+    this._close();
+  }
+
+  /**
+   * data-set 전체를 삭제합니다.
+   * @private
+   */
+  private _removeAllData() {
+    const msgBox = new Messagebox();
+    msgBox.message = '데이터 Set 내역을 모두 삭제하시겠습니까?';
+    msgBox.options = {
+      id: 'msgBox5',
+      align: 'center',
+      icon: 'question'
+    };
+    msgBox.show();
+
+    msgBox.yes(() => {
+      const list = JSON.parse(localStorage.dataSet);
+      const dataSet: DataSet | void = this.findDataSet(list);
+
+      list.splice(list.indexOf(dataSet), 1);
+
+      localStorage.setItem('dataSet', JSON.stringify(list));
+
+      this._renderDataList();
+    });
+  }
+
+  /**
+   * data-set을 삭제합니다.
+   * @param data
+   * @private
+   */
+  private _reomveData(data: SearchData[], index: number) {
+    const list = JSON.parse(localStorage.dataSet);
+
+    const dataSet: DataSet | void = this.findDataSet(list);
+
+    if (dataSet) {
+      dataSet.data.splice(index, 1);
+    }
+
+    localStorage.setItem('dataSet', JSON.stringify(list));
+    this._renderDataList();
+  }
+
+  /**
+   * reset 버튼 클릭시 처리
+   *
+   */
+  public _resetClick() {
+    for (let i = 0; i < this.contentList.length; i++) {
+      const control: any = this.contentList[i];
+      control.value = '';
+    }
+  }
+
+  private findDataSet(list: DataSet[]): DataSet | void {
+    for (let i = 0; i < list.length; i++) {
+      const data = list[i];
+      if (data.userId === userId && data.menuId === menuId && data.containerId === this.$parent!.id) {
+        return data;
+      }
+    }
+  }
+
+  /**
+   * capture 버튼 클릭시 처리
+   * @private
+   */
+  public _captureClick() {
+    const searchData: SearchData[] = [];
+
+    const defalut = {
+      userId: userId,
+      menuId: menuId,
+      containerId: this.$parent!.id,
+      data: []
+    };
+
+    let dataSetList: DataSet[];
+    let dataSet: DataSet | void;
+    let save = false;
+
+    if (!localStorage.dataSet) {
+      dataSetList = [defalut];
+    } else {
+      dataSetList = JSON.parse(localStorage.dataSet);
+    }
+
+    dataSet = this.findDataSet(dataSetList);
+
+    if (!dataSet) {
+      dataSet = defalut;
+      dataSetList.push(dataSet);
+    }
+
+    for (let i = 0; i < this.contentList.length; i++) {
+      const control: any = this.contentList[i];
+      const id = control.id || '';
+      const value = control.value || '';
+      const title = control.title || '';
+      // if (id && value) {
+      searchData.push({
+        id: id,
+        title: title,
+        value: value,
+        dateTime: date.format(date.parse(new Date()), 'yyyy-MM-dd HH:mm:ss')
+      });
+      save = true;
+      // }
+    }
+
+    const count = dataSet.data.unshift(searchData);
+    if (count > 30) {
+      dataSet.data.pop();
+    }
+
+    if (save) {
+      localStorage.setItem('dataSet', JSON.stringify(dataSetList));
+      this.snackbar?.classList.remove('fadeout');
+      this.snackbar?.classList.add('fadein');
+
+      // 스낵바가 사라질 때 애니메이션을 주기위한 class 변경
+      setTimeout(() => {
+        this.snackbar?.classList.remove('fadein');
+        this.snackbar?.classList.add('fadeout');
+      }, 2000);
     }
   }
 
