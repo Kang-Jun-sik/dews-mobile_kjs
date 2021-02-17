e5d216bbf914472cd1dbedbfe231943dccd5f009
구영준 <kuyoungjun@douzone.com>
2021-02-17 14:11:46 +0900

    서치컨테이너: set-data 수정

--- stories/dews-formcontainer720.stories.ts
+++ stories/dews-formcontainer720.stories.ts
@@ -13,9 +13,32 @@ export const FormContainer720 = () => html`<div style="width: 720px">
       </container-button>
       <container-content>
         <form-section title="기본정보">
-          <dews-textbox id="tbx01" title="1"></dews-textbox>
-          <dews-textbox id="tbx02" title="2"></dews-textbox>
-          <dews-textbox id="tbx03" title="3"></dews-textbox>
+          <dews-textbox id="tbx01" title="TextBox"></dews-textbox>
+          <dews-numerictextbox id="num01" title="numerictextbox">
+            <numericbox-button step="500"></numericbox-button>
+          </dews-numerictextbox>
+          <dews-masktextbox id="masktbx1" type="text" title="000-000" mask="000-000"></dews-masktextbox>
+          <dews-radiobutton-group title="radiobutton">
+            <dews-radiobutton label="button1"></dews-radiobutton>
+            <dews-radiobutton label="button2"></dews-radiobutton>
+            <dews-radiobutton label="button3"></dews-radiobutton>
+          </dews-radiobutton-group>
+          <dews-checkbox-group title="checkbox">
+            <dews-checkbox title="Check Box2 reverse" reverse> </dews-checkbox>
+            <dews-checkbox title="Check Box3"> </dews-checkbox>
+            <dews-checkbox title="Check Box4 "> </dews-checkbox>
+          </dews-checkbox-group>
+          <dews-datepicker id="date01" title="datepicker"></dews-datepicker>
+          <dews-timepicker id="time01" title="timepicker"> </dews-timepicker>
+          <dews-yearpicker id="year01" title="yearpicker"> </dews-yearpicker>
+          <dews-periodpicker id="period1" required title="PeriodPicker"> </dews-periodpicker>
+          <dews-weekperiodpicker id="period2" title="WeekPeriodPicker"> </dews-weekperiodpicker>
+          <dews-monthperiodpicker id="period3" title="MonthPeriodPicker"> </dews-monthperiodpicker>
+          <dews-dropdownlist id="ddl02" title="DropDown">
+            <dropdownlist-item title="DATA-1"></dropdownlist-item>
+            <dropdownlist-item title="DATA-2"></dropdownlist-item>
+            <dropdownlist-item title="DATA-3"></dropdownlist-item>
+          </dews-dropdownlist>
         </form-section>
         <form-section title="추가정보">
           <dews-textbox id="tbx04" title="4"></dews-textbox>
