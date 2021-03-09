eb4eba73a7da7cb2bb407832380ef33932859983
김현홍 <hhkim26@douzone.com>
2021-03-09 10:59:27 +0900

    네이티브앱에서 토큰받아오는 로직 수정

--- src/app/env/mobileApp/MobileAppAuthenticationManager.ts
+++ src/app/env/mobileApp/MobileAppAuthenticationManager.ts
@@ -20,15 +20,27 @@ export class MobileAppAuthenticationManager implements AuthenticationManagerInte
   }
 
   async authenticate(): Promise<void> {
+    // func_getHeaderAccessToken함수를 호출하면, 네이티브에서 dz_getHeaderAccessToken함수 호출로 token 전달
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    window.DzMobileBridge.dz_getHeaderAccessToken = (token: string) => {
+      this._accessToken = token;
+    };
+
+    // func_getHeaderDetailToken 호출하면, 네이티브에서 dz_getHeaderDetailToken 호출로 token 전달
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    window.DzMobileBridge.dz_getHeaderDetailToken = (token: string) => {
+      this._detailToken = token;
+    };
     // nativeApp에서 토큰을 받아오는 API호출
-    this._accessToken = window.DzMobileBridge.func_getHeaderAccessToken!();
-    this._detailToken = window.DzMobileBridge.func_getHeaderDetailToken!();
+    window.DzMobileBridge.func_getHeaderAccessToken!();
+    window.DzMobileBridge.func_getHeaderDetailToken!();
 
     // 세션에 불러온 토큰값 저장
     AuthenticateService.setAuthorizedToken(this._accessToken);
     AuthenticateService.setAuthorizedDetailToken(this._detailToken);
 
     // 토큰이 있으면 인증완료
+    // this._isAuthenticated = !!this._accessToken;
     this._isAuthenticated = true;
 
     alert(this._accessToken);
