bef87fbc665157b66d25e3be15317e6fc3ca42bd
jkoues <jkoues@douzone.com>
2021-03-10 17:35:41 +0900

    scroll 변경 함수 추가

--- src/components/base/DewsLayoutComponent.ts
+++ src/components/base/DewsLayoutComponent.ts
@@ -3,4 +3,14 @@ import { DewsComponent } from './DewsComponent.js';
  * Layout Component 관련
  *  - Area, Panel 등등
  */
-export class DewsLayoutComponent extends DewsComponent {}
+export class DewsLayoutComponent extends DewsComponent {
+  private headerHeight = 53;
+  private areaDividerHeight = 12;
+  scrollIntoView() {
+    window.scrollBy({
+      top: this.getBoundingClientRect().y - this.headerHeight - this.areaDividerHeight,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
+}
