e8f924508755d69da3555241996d2f4165d26933
kotiger <kotiger@douzone.com>
2021-03-16 10:39:40 +0900

    khj - scss 정리 (messagebox , snackbar)

--- stories/dews-messagebox.stories.ts
+++ stories/dews-messagebox.stories.ts
@@ -7,273 +7,278 @@ export default {
   title: 'Messagebox'
 };
 
-export const Messagebox1 = () => html`<div style="width: 360px">
-  <dews-box id="box1" title="box1">
-    <dews-container>
-      <dews-textbox title="default" @input="()=>{console.log('input');}"></dews-textbox>
-      <dews-textbox title="readonly" readonly></dews-textbox>
-      <button
-        id="msgBox1"
-        @click="${() => {
-          new Messagebox('저장이 완료되었습니다.', 'alert', {
-            id: 'msgBox1',
-            align: 'center',
-            icon: 'success'
-          })
-            .done(() => {
-              console.log('icon: success done');
-            })
-            .yes(() => {
-              console.log('icon: success yes');
-            })
-            .no(() => {
-              console.log('icon: success no');
-            });
-        }}"
-      >
-        icon: success
-      </button>
-    </dews-container>
-  </dews-box>
-  <dews-area-panel id="panel1">
-    <area-item id="item1" col="4">
-      <dews-box id="box2" title="box2">
-        <dews-textbox title="default"></dews-textbox>
+export const Messagebox1 = () => html`<style>
+    html {
+      font-size: 15px;
+    }
+  </style>
+  <div style="width: 360px">
+    <dews-box id="box1" title="box1">
+      <dews-container>
+        <dews-textbox title="default" @input="()=>{console.log('input');}"></dews-textbox>
         <dews-textbox title="readonly" readonly></dews-textbox>
         <button
-          id="msgBox2"
+          id="msgBox1"
           @click="${() => {
-            new Messagebox('조건에 해당하는 데이터가 없습니다.', 'alert', {
-              id: 'msgBox2',
+            new Messagebox('저장이 완료되었습니다.', 'alert', {
+              id: 'msgBox1',
               align: 'center',
-              icon: 'info'
+              icon: 'success'
             })
               .done(() => {
-                console.log('icon: info done');
+                console.log('icon: success done');
               })
               .yes(() => {
-                console.log('icon: info yes');
+                console.log('icon: success yes');
               })
               .no(() => {
-                console.log('icon: info no');
+                console.log('icon: success no');
               });
           }}"
         >
-          icon: info
+          icon: success
         </button>
-      </dews-box>
-    </area-item>
-    <area-item id="item2" col="8">
-      <dews-area-panel>
-        <area-item col="6">
-          <dews-box id="box3" title="box3">
-            <dews-textbox title="default"></dews-textbox>
-            <dews-textbox title="readonly" readonly></dews-textbox>
-            <button
-              id="msgBox3"
-              @click="${() => {
-                new Messagebox('서버와 연결이 끊겼습니다.', 'error', {
-                  id: 'msgBox3',
-                  align: 'center',
-                  icon: 'error'
+      </dews-container>
+    </dews-box>
+    <dews-area-panel id="panel1">
+      <area-item id="item1" col="4">
+        <dews-box id="box2" title="box2">
+          <dews-textbox title="default"></dews-textbox>
+          <dews-textbox title="readonly" readonly></dews-textbox>
+          <button
+            id="msgBox2"
+            @click="${() => {
+              new Messagebox('조건에 해당하는 데이터가 없습니다.', 'alert', {
+                id: 'msgBox2',
+                align: 'center',
+                icon: 'info'
+              })
+                .done(() => {
+                  console.log('icon: info done');
                 })
-                  .done(() => {
-                    console.log('icon: error done');
-                  })
-                  .yes(() => {
-                    console.log('icon: error yes');
-                  })
-                  .no(() => {
-                    console.log('icon: error no');
-                  });
-              }}"
-            >
-              icon: error
-            </button>
-          </dews-box>
-        </area-item>
-        <area-item col="6">
-          <dews-box id="box4" title="box4">
-            <dews-textbox title="default"></dews-textbox>
-            <dews-textbox title="readonly" readonly></dews-textbox>
-            <button
-              id="msgBox4"
-              @click="${() => {
-                new Messagebox('저장되지 않은 정보가 있습니다.\n그래도 진행하시겠습니까?', 'confirm', {
-                  id: 'msgBox4',
-                  align: 'center',
-                  icon: 'warning',
-                  cancel: true
+                .yes(() => {
+                  console.log('icon: info yes');
                 })
-                  .done(() => {
-                    console.log('icon: warning/cancel: true done');
+                .no(() => {
+                  console.log('icon: info no');
+                });
+            }}"
+          >
+            icon: info
+          </button>
+        </dews-box>
+      </area-item>
+      <area-item id="item2" col="8">
+        <dews-area-panel>
+          <area-item col="6">
+            <dews-box id="box3" title="box3">
+              <dews-textbox title="default"></dews-textbox>
+              <dews-textbox title="readonly" readonly></dews-textbox>
+              <button
+                id="msgBox3"
+                @click="${() => {
+                  new Messagebox('서버와 연결이 끊겼습니다.', 'error', {
+                    id: 'msgBox3',
+                    align: 'center',
+                    icon: 'error'
                   })
-                  .yes(() => {
-                    console.log('icon: warning/cancel: true yes');
+                    .done(() => {
+                      console.log('icon: error done');
+                    })
+                    .yes(() => {
+                      console.log('icon: error yes');
+                    })
+                    .no(() => {
+                      console.log('icon: error no');
+                    });
+                }}"
+              >
+                icon: error
+              </button>
+            </dews-box>
+          </area-item>
+          <area-item col="6">
+            <dews-box id="box4" title="box4">
+              <dews-textbox title="default"></dews-textbox>
+              <dews-textbox title="readonly" readonly></dews-textbox>
+              <button
+                id="msgBox4"
+                @click="${() => {
+                  new Messagebox('저장되지 않은 정보가 있습니다.\n그래도 진행하시겠습니까?', 'confirm', {
+                    id: 'msgBox4',
+                    align: 'center',
+                    icon: 'warning',
+                    cancel: true
                   })
-                  .no(() => {
-                    console.log('icon: warning/cancel: true no');
-                  });
-              }}"
-            >
-              icon: warning/cancel: true
-            </button>
-          </dews-box>
-        </area-item>
-      </dews-area-panel>
-    </area-item>
-  </dews-area-panel>
-  <dews-box id="box5" title="box5">
-    <dews-textbox title="default"></dews-textbox>
-    <dews-textbox title="readonly" readonly></dews-textbox>
-    <button
-      id="msgBox5"
-      @click="${() => {
-        new Messagebox('변경된 사항이 있습니다.\n저장하시겠습니까?', 'error', {
-          id: 'msgBox5',
-          align: 'center',
-          icon: 'question'
-        })
-          .done(() => {
-            console.log('icon: question done');
+                    .done(() => {
+                      console.log('icon: warning/cancel: true done');
+                    })
+                    .yes(() => {
+                      console.log('icon: warning/cancel: true yes');
+                    })
+                    .no(() => {
+                      console.log('icon: warning/cancel: true no');
+                    });
+                }}"
+              >
+                icon: warning/cancel: true
+              </button>
+            </dews-box>
+          </area-item>
+        </dews-area-panel>
+      </area-item>
+    </dews-area-panel>
+    <dews-box id="box5" title="box5">
+      <dews-textbox title="default"></dews-textbox>
+      <dews-textbox title="readonly" readonly></dews-textbox>
+      <button
+        id="msgBox5"
+        @click="${() => {
+          new Messagebox('변경된 사항이 있습니다.\n저장하시겠습니까?', 'error', {
+            id: 'msgBox5',
+            align: 'center',
+            icon: 'question'
           })
-          .yes(() => {
-            console.log('icon: question yes');
+            .done(() => {
+              console.log('icon: question done');
+            })
+            .yes(() => {
+              console.log('icon: question yes');
+            })
+            .no(() => {
+              console.log('icon: question no');
+            });
+        }}"
+      >
+        icon: question
+      </button>
+    </dews-box>
+    <dews-box id="box6" title="box6">
+      <dews-textbox title="default"></dews-textbox>
+      <dews-textbox title="readonly" readonly></dews-textbox>
+      <button
+        id="msgBox6"
+        @click="${() => {
+          new Messagebox('해당 앱을 기본으로 설정하시겠습니까?', 'alert', {
+            id: 'msgBox6',
+            align: 'center',
+            icon: 'question',
+            showCheckbox: 'never'
           })
-          .no(() => {
-            console.log('icon: question no');
-          });
-      }}"
-    >
-      icon: question
-    </button>
-  </dews-box>
-  <dews-box id="box6" title="box6">
-    <dews-textbox title="default"></dews-textbox>
-    <dews-textbox title="readonly" readonly></dews-textbox>
-    <button
-      id="msgBox6"
-      @click="${() => {
-        new Messagebox('해당 앱을 기본으로 설정하시겠습니까?', 'alert', {
-          id: 'msgBox6',
-          align: 'center',
-          icon: 'question',
-          showCheckbox: 'never'
-        })
-          .done(() => {
-            console.log('icon: question/다시보지않기 done');
+            .done(() => {
+              console.log('icon: question/다시보지않기 done');
+            })
+            .yes(() => {
+              console.log('icon: question/다시보지않기 yes');
+            })
+            .no(() => {
+              console.log('icon: question/다시보지않기 no');
+            });
+        }}"
+      >
+        icon: question/다시보지않기
+      </button>
+    </dews-box>
+    <dews-box id="box7" title="box7">
+      <dews-textbox title="default"></dews-textbox>
+      <dews-textbox title="readonly" readonly></dews-textbox>
+      <button
+        id="msgBox7"
+        @click="${() => {
+          new Messagebox('해당 앱을 기본으로 설정하시겠습니까?', 'confirm', {
+            id: 'msgBox7',
+            align: 'center',
+            icon: 'question',
+            showCheckbox: 'today'
           })
-          .yes(() => {
-            console.log('icon: question/다시보지않기 yes');
+            .done(() => {
+              console.log('icon: question/오늘하루보지않기 done');
+            })
+            .yes(() => {
+              console.log('icon: question/오늘하루보지않기 yes');
+            })
+            .no(() => {
+              console.log('icon: question/오늘하루보지않기 no');
+            });
+        }}"
+      >
+        icon: question/오늘하루보지않기
+      </button>
+    </dews-box>
+    <dews-box id="box8" title="box8">
+      <dews-textbox title="default"></dews-textbox>
+      <dews-textbox title="readonly" readonly></dews-textbox>
+      <button
+        id="msgBox8"
+        @click="${() => {
+          new Messagebox('조건에 해당하는 데이터가 없습니다.', 'alert', {
+            id: 'msgBox8',
+            align: 'center',
+            icon: 'info',
+            cancel: true
           })
-          .no(() => {
-            console.log('icon: question/다시보지않기 no');
-          });
-      }}"
-    >
-      icon: question/다시보지않기
-    </button>
-  </dews-box>
-  <dews-box id="box7" title="box7">
-    <dews-textbox title="default"></dews-textbox>
-    <dews-textbox title="readonly" readonly></dews-textbox>
-    <button
-      id="msgBox7"
-      @click="${() => {
-        new Messagebox('해당 앱을 기본으로 설정하시겠습니까?', 'confirm', {
-          id: 'msgBox7',
-          align: 'center',
-          icon: 'question',
-          showCheckbox: 'today'
-        })
-          .done(() => {
-            console.log('icon: question/오늘하루보지않기 done');
+            .done(() => {
+              console.log('icon: info/cancel: true done');
+            })
+            .yes(() => {
+              console.log('icon: info/cancel: true yes');
+            })
+            .no(() => {
+              console.log('icon: info/cancel: true no');
+            });
+        }}"
+      >
+        icon: info/cancel: true
+      </button>
+    </dews-box>
+    <dews-box id="box9" title="box9">
+      <dews-textbox title="default"></dews-textbox>
+      <dews-textbox title="readonly" readonly></dews-textbox>
+      <button
+        id="msgBox9"
+        @click="${() => {
+          new Messagebox('저장되지 않은 데이터가 있습니다.\n그래도 진행하시겠습니까?', 'error', {
+            id: 'msgBox9',
+            align: 'left',
+            icon: 'warning'
           })
-          .yes(() => {
-            console.log('icon: question/오늘하루보지않기 yes');
+            .done(() => {
+              console.log('icon: warning/align: left done');
+            })
+            .yes(() => {
+              console.log('icon: warning/align: left yes');
+            })
+            .no(() => {
+              console.log('icon: warning/align: left no');
+            });
+        }}"
+      >
+        icon: warning/align: left
+      </button>
+    </dews-box>
+    <dews-box id="box10" title="box10">
+      <dews-textbox title="default"></dews-textbox>
+      <dews-textbox title="readonly" readonly></dews-textbox>
+      <button
+        id="msgBox10"
+        @click="${() => {
+          new Messagebox('저장되지 않은 데이터가 있습니다.\n그래도 진행하시겠습니까?', 'confirm', {
+            id: 'msgBox10',
+            align: 'right',
+            icon: 'warning'
           })
-          .no(() => {
-            console.log('icon: question/오늘하루보지않기 no');
-          });
-      }}"
-    >
-      icon: question/오늘하루보지않기
-    </button>
-  </dews-box>
-  <dews-box id="box8" title="box8">
-    <dews-textbox title="default"></dews-textbox>
-    <dews-textbox title="readonly" readonly></dews-textbox>
-    <button
-      id="msgBox8"
-      @click="${() => {
-        new Messagebox('조건에 해당하는 데이터가 없습니다.', 'alert', {
-          id: 'msgBox8',
-          align: 'center',
-          icon: 'info',
-          cancel: true
-        })
-          .done(() => {
-            console.log('icon: info/cancel: true done');
-          })
-          .yes(() => {
-            console.log('icon: info/cancel: true yes');
-          })
-          .no(() => {
-            console.log('icon: info/cancel: true no');
-          });
-      }}"
-    >
-      icon: info/cancel: true
-    </button>
-  </dews-box>
-  <dews-box id="box9" title="box9">
-    <dews-textbox title="default"></dews-textbox>
-    <dews-textbox title="readonly" readonly></dews-textbox>
-    <button
-      id="msgBox9"
-      @click="${() => {
-        new Messagebox('저장되지 않은 데이터가 있습니다.\n그래도 진행하시겠습니까?', 'error', {
-          id: 'msgBox9',
-          align: 'left',
-          icon: 'warning'
-        })
-          .done(() => {
-            console.log('icon: warning/align: left done');
-          })
-          .yes(() => {
-            console.log('icon: warning/align: left yes');
-          })
-          .no(() => {
-            console.log('icon: warning/align: left no');
-          });
-      }}"
-    >
-      icon: warning/align: left
-    </button>
-  </dews-box>
-  <dews-box id="box10" title="box10">
-    <dews-textbox title="default"></dews-textbox>
-    <dews-textbox title="readonly" readonly></dews-textbox>
-    <button
-      id="msgBox10"
-      @click="${() => {
-        new Messagebox('저장되지 않은 데이터가 있습니다.\n그래도 진행하시겠습니까?', 'confirm', {
-          id: 'msgBox10',
-          align: 'right',
-          icon: 'warning'
-        })
-          .done(() => {
-            console.log('icon: warning/align: right done');
-          })
-          .yes(() => {
-            console.log('icon: warning/align: right yes');
-          })
-          .no(() => {
-            console.log('icon: warning/align: right no');
-          });
-      }}"
-    >
-      icon: warning/align: right
-    </button>
-  </dews-box>
-</div> `;
+            .done(() => {
+              console.log('icon: warning/align: right done');
+            })
+            .yes(() => {
+              console.log('icon: warning/align: right yes');
+            })
+            .no(() => {
+              console.log('icon: warning/align: right no');
+            });
+        }}"
+      >
+        icon: warning/align: right
+      </button>
+    </dews-box>
+  </div> `;
--- stories/dews-snackbar.stories.ts
+++ stories/dews-snackbar.stories.ts
@@ -7,104 +7,109 @@ export default {
   title: 'Snackbar'
 };
 
-export const Snackbar1 = () => html`<div style="width: 360px">
-  <p style="height: 200px;">
-    <button
-      @click="${() => {
-        new Snackbar('승인신청이 접수되었습니다.').done(() => {
-          console.log('승인 done');
-        });
-      }}"
-    >
-      승인
-    </button>
-  </p>
-  <p style="height: 200px;">
-    <button
-      @click="${() => {
-        new Snackbar('저장되었습니다.', false).done(() => {
-          console.log('아이콘 미출력 done');
-        });
-      }}"
-    >
-      아이콘 미출력
-    </button>
-  </p>
-  <p style="height: 200px;">
-    <button
-      @click="${() => {
-        new Snackbar('대기중인 업데이트 정보가 있습니다.', 'info').done(() => {
-          console.log('정보1 done');
-        });
-      }}"
-    >
-      정보1
-    </button>
-  </p>
-  <p style="height: 200px;">
-    <button
-      @click="${() => {
-        new Snackbar('준비중입니다.', { icon: 'info' }).done(() => {
-          console.log('정보2 done');
-        });
-      }}"
-    >
-      정보2
-    </button>
-  </p>
-  <p style="height: 200px;">
-    <button
-      @click="${() => {
-        new Snackbar('잘못 입력하셨습니다.', 'warning').done(() => {
-          console.log('경고 done');
-        });
-      }}"
-    >
-      경고
-    </button>
-  </p>
-  <p style="height: 200px;">
-    <button
-      @click="${() => {
-        new Snackbar('조건에 해당하는 데이터가 없습니다.', { icon: 'warning', exposureTime: 5 }).done(() => {
-          console.log('경고 5초 done');
-        });
-      }}"
-    >
-      경고 5초
-    </button>
-  </p>
-  <p style="height: 200px;">
-    <button
-      @click="${() => {
-        new Snackbar('치명적인 버그가 발생했습니다.', 'error').done(() => {
-          console.log('에러 done');
-        });
-      }}"
-    >
-      에러
-    </button>
-  </p>
-  <p style="height: 200px;">
-    <button
-      @click="${() => {
-        new Snackbar('작업이 처리되지 않았습니다.', { icon: 'error', exposureTime: 7 }).done(() => {
-          console.log('에러 7초 done');
-        });
-      }}"
-    >
-      에러 7초
-    </button>
-  </p>
-  <p style="height: 200px;">
-    <button
-      @click="${() => {
-        new Snackbar('다운로드를 준비하고 있습니다.', 'loading').done(() => {
-          console.log('로딩 done');
-        });
-      }}"
-    >
-      로딩
-    </button>
-  </p>
-</div>`;
+export const Snackbar1 = () => html` <style>
+    html {
+      font-size: 15px;
+    }
+  </style>
+  <div style="width: 360px">
+    <p style="height: 200px;">
+      <button
+        @click="${() => {
+          new Snackbar('승인신청이 접수되었습니다.').done(() => {
+            console.log('승인 done');
+          });
+        }}"
+      >
+        승인
+      </button>
+    </p>
+    <p style="height: 200px;">
+      <button
+        @click="${() => {
+          new Snackbar('저장되었습니다.', false).done(() => {
+            console.log('아이콘 미출력 done');
+          });
+        }}"
+      >
+        아이콘 미출력
+      </button>
+    </p>
+    <p style="height: 200px;">
+      <button
+        @click="${() => {
+          new Snackbar('대기중인 업데이트 정보가 있습니다.', 'info').done(() => {
+            console.log('정보1 done');
+          });
+        }}"
+      >
+        정보1
+      </button>
+    </p>
+    <p style="height: 200px;">
+      <button
+        @click="${() => {
+          new Snackbar('준비중입니다.', { icon: 'info' }).done(() => {
+            console.log('정보2 done');
+          });
+        }}"
+      >
+        정보2
+      </button>
+    </p>
+    <p style="height: 200px;">
+      <button
+        @click="${() => {
+          new Snackbar('잘못 입력하셨습니다.', 'warning').done(() => {
+            console.log('경고 done');
+          });
+        }}"
+      >
+        경고
+      </button>
+    </p>
+    <p style="height: 200px;">
+      <button
+        @click="${() => {
+          new Snackbar('조건에 해당하는 데이터가 없습니다.', { icon: 'warning', exposureTime: 5 }).done(() => {
+            console.log('경고 5초 done');
+          });
+        }}"
+      >
+        경고 5초
+      </button>
+    </p>
+    <p style="height: 200px;">
+      <button
+        @click="${() => {
+          new Snackbar('치명적인 버그가 발생했습니다.', 'error').done(() => {
+            console.log('에러 done');
+          });
+        }}"
+      >
+        에러
+      </button>
+    </p>
+    <p style="height: 200px;">
+      <button
+        @click="${() => {
+          new Snackbar('작업이 처리되지 않았습니다.', { icon: 'error', exposureTime: 7 }).done(() => {
+            console.log('에러 7초 done');
+          });
+        }}"
+      >
+        에러 7초
+      </button>
+    </p>
+    <p style="height: 200px;">
+      <button
+        @click="${() => {
+          new Snackbar('다운로드를 준비하고 있습니다.', 'loading').done(() => {
+            console.log('로딩 done');
+          });
+        }}"
+      >
+        로딩
+      </button>
+    </p>
+  </div>`;
