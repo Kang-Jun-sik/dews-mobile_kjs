b0ea35a05ac84ba510e576692566bdf38ee98160
kotiger <kotiger@douzone.com>
2021-03-17 09:34:06 +0900

    khj - slider will-touch 추가, stories 에 font 추가

--- stories/dews-loading.stories.ts
+++ stories/dews-loading.stories.ts
@@ -7,56 +7,64 @@ export default {
   title: 'Loading'
 };
 
-export const Loading1 = () => html`<div style="width: 360px">
-  <button
-    @click="${() => {
-      showLoading({ message: 'Loading' });
-    }}"
-  >
-    showLoading 5초
-  </button>
-  <br />
-  <div style="border: 1px solid red; width: 300px; height: 500px;" id="redDiv"></div>
-  <button
-    @click="${() => {
-      showLoading({
-        message: 'Loading in red div tag',
-        target: document.querySelector('#redDiv') as HTMLElement
-      });
-    }}"
-  >
-    showLoadingInRedDiv
-  </button>
-  <div style="border: 1px solid blue; width: 300px; height: 600px;" id="blueDiv">
-    <div style="border: 2px solid yellowgreen; width: 200px; height: 400px;" id="greenDiv"></div>
-  </div>
-  <button
-    @click="${() => {
-      showLoading({ message: 'Loading in blue div tag', target: document.querySelector('#blueDiv') as HTMLElement });
-    }}"
-  >
-    showLoadingInBlueDiv
-  </button>
-  <br />
-  <button
-    @click="${() => {
-      showLoading({ message: 'Loading in green div tag', target: document.querySelector('#greenDiv') as HTMLElement });
-    }}"
-  >
-    showLoadingInGreenDiv
-  </button>
-  <div style="border: 1px solid purple; width: 300px; height: 500px;" id="purpleDiv"></div>
-  <button
-    @click="${() => {
-      showLoading({
-        message: 'Loading in purple div tag',
-        target: document.querySelector('#purpleDiv') as HTMLElement
-      });
-    }}"
-  >
-    showLoadingInPurpleDiv
-  </button>
-</div>`;
+export const Loading1 = () => html`<style>
+    html {
+      font-size: 15px;
+    }
+  </style>
+  <div style="width: 360px">
+    <button
+      @click="${() => {
+        showLoading({ message: 'Loading' });
+      }}"
+    >
+      showLoading 5초
+    </button>
+    <br />
+    <div style="border: 1px solid red; width: 300px; height: 500px;" id="redDiv"></div>
+    <button
+      @click="${() => {
+        showLoading({
+          message: 'Loading in red div tag',
+          target: document.querySelector('#redDiv') as HTMLElement
+        });
+      }}"
+    >
+      showLoadingInRedDiv
+    </button>
+    <div style="border: 1px solid blue; width: 300px; height: 600px;" id="blueDiv">
+      <div style="border: 2px solid yellowgreen; width: 200px; height: 400px;" id="greenDiv"></div>
+    </div>
+    <button
+      @click="${() => {
+        showLoading({ message: 'Loading in blue div tag', target: document.querySelector('#blueDiv') as HTMLElement });
+      }}"
+    >
+      showLoadingInBlueDiv
+    </button>
+    <br />
+    <button
+      @click="${() => {
+        showLoading({
+          message: 'Loading in green div tag',
+          target: document.querySelector('#greenDiv') as HTMLElement
+        });
+      }}"
+    >
+      showLoadingInGreenDiv
+    </button>
+    <div style="border: 1px solid purple; width: 300px; height: 500px;" id="purpleDiv"></div>
+    <button
+      @click="${() => {
+        showLoading({
+          message: 'Loading in purple div tag',
+          target: document.querySelector('#purpleDiv') as HTMLElement
+        });
+      }}"
+    >
+      showLoadingInPurpleDiv
+    </button>
+  </div>`;
 
 function showLoading(options: LoadingOptions) {
   const loading = new Loading({
